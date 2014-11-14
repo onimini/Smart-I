19389e031e7557c2587943b369e40ac96bc7649f
onimini <s7070059@kmitl.ac.th>
2014-11-14 12:31:39 +0700

    Update Project_Smart-I.py
    Change something for short code.

--- Project_Smart-I.py
+++ Project_Smart-I.py
@@ -1,12 +1,11 @@
 '''Calculate score for Smart-I examination.'''
 def score(math, eng, read, social):
-    '''input score of subject in parameter
+    '''Input scores of subject in parameter
     then calculate score from percent of subject which
-    math eng and read will multi with 0.3
-    social multi with 0.1 then find all plus.'''
-    calculate = (math + eng + read) * 0.3
-    total = social * 0.1
-    summary = calculate + total
+    Mathematic(math) English(eng) and Reading(read) 
+    will multi with 0.3, Social multi with 0.1 
+    then find all plus.'''
+    summary = ((math + eng + read) * 0.3 + (social * 0.1))
     if summary >= 63:
         print str(summary) + ' Maybe pass the exam.'
     else:
