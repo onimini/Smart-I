a35357503a52a406f41864113aacca35cc136348
PatittaS <s7070065@kmitl.ac.th>
2014-11-14 22:49:00 +0700

    Update Project_Smart-I.py

--- Project_Smart-I.py
+++ Project_Smart-I.py
@@ -1,13 +1,20 @@
 '''Calculate score for Smart-I examination.'''
+from Tkinter import *
+class Smart(object):
+    def __init__(score):
+        score.root = Tk()
+        score.frame = Frame(score.root).pack()
+'''
 def score(math, eng, read, social):
-    '''Input scores of subject in parameter
+    ''''''Input scores of subject in parameter
     then calculate score from percent of subject which
     Mathematic(math) English(eng) and Reading(read) 
     will multi with 0.3, Social multi with 0.1 
-    then find all plus.'''
+    then find all plus.''''''
     summary = ((math + eng + read) * 0.3 + (social * 0.1))
     if summary >= 63:
         print str(summary) + ' Maybe pass the exam.'
     else:
         print str(summary) + ' Maybe not pass the exam.'
 score(input(), input(), input(), input())
+'''
