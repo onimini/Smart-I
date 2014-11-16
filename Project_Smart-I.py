bd576e9a261dc1d6cdede7155bb68af3a1742a01
PatittaS <s7070065@kmitl.ac.th>
2014-11-16 16:45:41 +0700

    Update Project_Smart-I.py

--- Project_Smart-I.py
+++ Project_Smart-I.py
@@ -1,9 +1,48 @@
 '''Calculate score for Smart-I examination.'''
 from Tkinter import *
+import tkMessageBox
+
 class Smart(object):
     def __init__(score):
         score.root = Tk()
-        score.frame = Frame(score.root).pack()
+        score.root.title("Smart-I")
+        score.label = Label(score.root, text="Enter your score.").grid(row=0)
+        
+        score.math = Label(score.root, text="Math").grid(row=1)
+        score.textmath = IntVar()
+        Entry(score.root, textvariable=score.textmath).grid(row=1, ccolumn=1)
+        
+        score.eng = Label(score.root, text="English").grid(row=2)
+        score.texteng = IntVar()
+        Entry(score.root, textvariable=score.texteng).grid(row=2, column=1)
+        
+        score.read = Label(score.root, text="Reading").grid(row=3)
+        score.textread = IntVar()
+        Entry(score.root, textvariable=score.textread).grid(row=3, column=1)
+        
+        score.know = Label(score.root, text="Knowledge").grid(row=4)
+        score.textknow = IntVar()
+        Entry(score.root, textvariable=score.textknow).grid(row=4, column=1)
+        
+        score.buttontext = IntVar()
+        score.buttontext.set("Calculate")
+        Button(score.root, textvariable=score.buttontext, command=score.calculate).grid(row=5, column=1)
+        
+        score.root.mainloop()
+        
+    def calculate(score):
+         '''Input scores of subject in parameter
+        then calculate score from percent of subject which
+        Mathematic(math) English(eng) and Reading(read) 
+        will multi with 0.3, Social multi with 0.1 
+        then find all plus.'''
+        input = score.textmath.get()
+        input = score.texteng.get()
+        input = score.textread.get()
+        input = score.textknow.get()
+        result = (score.textmath.get() + score.texteng.get() + score.textread.get())*0.3 + (score.textknow.get()*0.1)
+        print result
+        
 '''
 def score(math, eng, read, social):
     ''''''Input scores of subject in parameter
