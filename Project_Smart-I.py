e62caeae674ed5feb05e00185a0b8d7be20dee15
onimini <s7070059@kmitl.ac.th>
2014-11-17 13:36:30 +0700

    Add Tkinter
    Add title and frame

--- Project_Smart-I.py
+++ Project_Smart-I.py
@@ -1,14 +1,29 @@
 '''Calculate score for Smart-I examination.'''
+
+from Tkinter import *
+root = Tk()
+root.title('Smart l')
+
+class GUI(object):
+    '''Show Graphic User Interface for running program 'Smart l'.'''
+    def __init__(self):
+        self.root = Tk()
+        self.frame = Frame(self.root)
+        self.frame.pack()
+
+        self.root.mainloop()
+
 def score(math, eng, read, social):
-    '''input score of subject in parameter
+    '''Input scores of subject in parameter
     then calculate score from percent of subject which
-    math eng and read will multi with 0.3
-    social multi with 0.1 then find all plus.'''
-    calculate = (math + eng + read) * 0.3
-    total = social * 0.1
-    summary = calculate + total
+    Mathematic(math) English(eng) and Reading(read) 
+    will multi with 0.3, Social multi with 0.1 
+    then find all plus.'''
+    summary = ((math + eng + read) * 0.3 + (social * 0.1))
     if summary >= 63:
         print str(summary) + ' Maybe pass the exam.'
     else:
         print str(summary) + ' Maybe not pass the exam.'
 score(input(), input(), input(), input())
+
+GUI()
