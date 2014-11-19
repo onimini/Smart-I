78336735b876d82dcf4e6d952163c8162ad50b3f
PatittaS <s7070065@kmitl.ac.th>
2014-11-19 22:02:50 +0700

    Add class and change GUI a little bit.

--- Project_Smart-I.py
+++ Project_Smart-I.py
@@ -5,6 +5,8 @@ import tkMessageBox
 class First(object):
     
     def __init__(score):
+        global book
+        book = {}
         score.root = Tk()
         score.root.geometry("340x250")
         score.root.title("Smart-I")
@@ -31,94 +33,78 @@ class First(object):
 
         score.buttontext = IntVar()
         score.buttontext.set("Calculate")
-        Button(score.root, textvariable=score.buttontext, command=score.cal)\
+        Button(score.root, textvariable=score.buttontext)\
                            .place(relx=0.5, rely=0.6, anchor=CENTER)
 
         score.root.mainloop()
 
-    def button(score):
+    def button(score1):
         
-        global lisObject
-        global lisTemp
-        lisObject = []
-        lisTemp = []
-        
-        score.root = Tk()
-        score.root.geometry("510x180")
+        score1.root = Tk()
+        score1.root.geometry("510x180")
         #score.root.configure(background='black')
-        score.root.title('Smart-I')
+        score1.root.title('Smart-I')
 
-        score.lalabel = Label(score.root, text=" ").grid(row=0)
-        score.label = Label(score.root, text="SMART-I", fg='red').place(relx=0.5, rely=0.08, anchor=CENTER)
+        score1.lalabel = Label(score1.root, text=" ").grid(row=0)
+        score1.label = Label(score1.root, text="SMART-I", fg='red').place(relx=0.5, rely=0.08, anchor=CENTER)
         
         '''Tap for your information.'''
-        score.label = Label(score.root, text="Enter your information.", fg='blue').grid(row=2)
+        score1.label = Label(score1.root, text="Enter your information.", fg='blue').grid(row=2)
         
-        score.name = Label(score.root, text="Name:").grid(row=3)
-        score.txtname = StringVar()
-        Entry(score.root, textvariable=score.txtname).grid(row=3, column=1)
+        score1.name = Label(score1.root, text="Name:").grid(row=3)
+        score1.txtname = StringVar()
+        Entry(score1.root, textvariable=score1.txtname).grid(row=3, column=1)
 
-        score.surname = Label(score.root, text="Surname:").grid(row=4)
-        score.txtsurname = StringVar()
-        Entry(score.root, textvariable=score.txtsurname).grid(row=4, column=1)
+        score1.surname = Label(score1.root, text="Surname:").grid(row=4)
+        score1.txtsurname = StringVar()
+        Entry(score1.root, textvariable=score1.txtsurname).grid(row=4, column=1)
 
-        score.school = Label(score.root, text="School:").grid(row=5)
-        score.txtschool = StringVar()
-        Entry(score.root, textvariable=score.txtschool).grid(row=5, column=1)
+        score1.school = Label(score1.root, text="School:").grid(row=5)
+        score1.txtschool = StringVar()
+        Entry(score1.root, textvariable=score1.txtschool).grid(row=5, column=1)
 
-        score.std_id = Label(score.root, text="Student ID:").grid(row=6)
-        score.txtID = IntVar()
-        Entry(score.root, textvariable=score.txtID).grid(row=6, column=1)
+        score1.std_id = Label(score1.root, text="Student ID:").grid(row=6)
+        score1.txtID = IntVar()
+        Entry(score1.root, textvariable=score1.txtID).grid(row=6, column=1)
 
-        score.lalabel = Label(score.root, text="    ").grid(column=2)
+        score1.lalabel = Label(score1.root, text="    ").grid(column=2)
 
         '''Tap for your scores.'''
-        score.label = Label(score.root, text="Enter your scores.", fg='blue').grid(row=2, column=3)
+        score1.label = Label(score1.root, text="Enter your scores.", fg='blue').grid(row=2, column=3)
 
-        score.math = Label(score.root, text="Math:").grid(row=3, column=3)
-        score.txtmath = IntVar()
-        Entry(score.root, textvariable=score.txtmath).grid(row=3, column=4)
+        score1.math = Label(score1.root, text="Math:").grid(row=3, column=3)
+        score1.txtmath = IntVar()
+        Entry(score1.root, textvariable=score1.txtmath).grid(row=3, column=4)
 
-        score.eng = Label(score.root, text="English:").grid(row=4, column=3)
-        score.txteng = IntVar()
-        Entry(score.root, textvariable=score.txteng).grid(row=4, column=4)
+        score1.eng = Label(score1.root, text="English:").grid(row=4, column=3)
+        score1.txteng = IntVar()
+        Entry(score1.root, textvariable=score1.txteng).grid(row=4, column=4)
 
-        score.read = Label(score.root, text="Reading:").grid(row=5, column=3)
-        score.txtread = IntVar()
-        Entry(score.root, textvariable=score.txtread).grid(row=5, column=4)
+        score1.read = Label(score1.root, text="Reading:").grid(row=5, column=3)
+        score1.txtread = IntVar()
+        Entry(score1.root, textvariable=score1.txtread).grid(row=5, column=4)
 
-        score.knowledge = Label(score.root, text="Knowledge:").grid(row=6, column=3)
-        score.txtknow = IntVar()
-        Entry(score.root, textvariable=score.txtknow).grid(row=6, column=4)
+        score1.knowledge = Label(score1.root, text="Knowledge:").grid(row=6, column=3)
+        score1.txtknow = IntVar()
+        Entry(score1.root, textvariable=score1.txtknow).grid(row=6, column=4)
         
         '''Button Save.'''
-        score.buttontext = IntVar()
-        Button(score.root, text="Save", textvariable=score.buttontext, command= score.new).place(relx=0.5, rely=0.9, anchor=CENTER)
+        score1.buttontext = Button(score1.root, text="Save", textvariable=score1.buttontext).place(relx=0.5, rely=0.9, anchor=CENTER)
 
-        score.lalabel = Label(score.root, text="").grid(row=8)
-        score.root.mainloop()
-
-    def cal(score):
-        score.root = Tk()
-        
-    def new(score):
-        std = student(score.txtID.get(),score.txtname.get(),score.txtsurname.get(),score.txtschool.get())              
-        lisObject.append(std)
-        lisTemp.append(int(score.txtID.get()))
-        
-    def slis(score):
-        xx = score.txt_in_id.get()
-        print lisObject[lisTemp.index(xx)].show()
-        
-class student():
-    def __init__(std,std_id,name,surname,age,school):
-        std.id = std_id
-        std.name = name
-        std.surname = surname
-        std.age = age
-        std.school = school  
-
-    def show(std):
-        print std.name,std.surname,std.age
+        score1.lalabel = Label(score1.root, text="").grid(row=8)
+##        def new(score):
+##            cally = stdent.calcu(score1.txtmath.get(), score1.txteng.get(), score1.txtread.get(), score1.txtknow.get())
+##            print cally
+##        
+##class student():
+##    def __init__(std, math, eng, read, know):
+##        std.math = math
+##        std.eng = eng
+##        std.read = read
+##        std.know = know
+##
+##    def calcu(std):
+##        result = ((std.math + std.eng + std.read)*0.3) + (std.know*0.1)
+##        print result
         
 First()
